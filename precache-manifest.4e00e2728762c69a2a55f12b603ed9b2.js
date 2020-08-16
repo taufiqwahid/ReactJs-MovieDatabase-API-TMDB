32137a586549589876d0f9bd8a2eb275b2f024a5
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-08-16 14:06:56 +0800

    Updates

---
+++ precache-manifest.4e00e2728762c69a2a55f12b603ed9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbce731b4f66a38d1057da9141962eb",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/index.html"
+  },
+  {
+    "revision": "89ec580b4e81dd646ddc",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/css/main.22bd4cd0.chunk.css"
+  },
+  {
+    "revision": "a1c0e78aa83fb39eb1a1",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/2.6f8880dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/2.6f8880dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ec580b4e81dd646ddc",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/main.c19e07fd.chunk.js"
+  },
+  {
+    "revision": "84394f5fac645d50f006",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/runtime-main.69a988fd.js"
+  }
+]);
