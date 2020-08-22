9c9a79b49a2ce501fb51ac311e6c9a1e610f8d50
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-08-23 05:05:01 +0800

    Updates

---
+++ precache-manifest.2417b6b1ad9c2ac01ab3ab4a6dc75e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4819ab5786e0dd6a2b38f53654c4f4",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/index.html"
+  },
+  {
+    "revision": "bd1ebfc31feef443eb32",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/css/main.49b2dc3c.chunk.css"
+  },
+  {
+    "revision": "361749de2755f0992232",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/2.b3eb9b8c.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/2.b3eb9b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1ebfc31feef443eb32",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/main.755b98de.chunk.js"
+  },
+  {
+    "revision": "84394f5fac645d50f006",
+    "url": "/ReactJs-MovieDatabase-API-TMDB/static/js/runtime-main.69a988fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactJs-MovieDatabase-API-TMDB/precache-manifest.c0982adff56d1f84eb352ff4544fd693.js"
+  "/ReactJs-MovieDatabase-API-TMDB/precache-manifest.2417b6b1ad9c2ac01ab3ab4a6dc75e0d.js"
 );
 
 self.addEventListener('message', (event) => {
